d5e003ba0f5e4302715e9f89a81187b722ddf2f7
Shukrullojon <shukrullobk@gmail.com>
2024-04-27 16:38:30 +0500

    'ok'

--- resources/views/trip/edit.blade.php
+++ resources/views/trip/edit.blade.php
@@ -24,7 +24,7 @@
         @endif
 
 
-        {!! Form::model($trip, ['method' => 'PATCH','route' => ['trip.update', $trip->id]]) !!}
+        {!! Form::model($trip, ['method' => 'PATCH','enctype' => 'multipart/form-data','route' => ['trip.update', $trip->id]]) !!}
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
